28eb579d70f39433bf2fed65c53a67a7d2e7833e
Christine Olukere <coolukere@msn.com>
2018-12-17 15:44:12 -0500

    changed the card body color and border

--- js-files/main.js
+++ js-files/main.js
@@ -5,7 +5,7 @@ let card = document.querySelectorAll(".card");
 for(let i=0; i<card.length; i++){
 
 
-card[i].style.backgroundColor+="lightBlue";
-card[i].style.border="2px solid yellow";
+card[i].style.backgroundColor+="#4D7BFD";
+card[i].style.border="2px solid #2F14A3";
 
 }
